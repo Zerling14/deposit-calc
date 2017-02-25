1f40ff2d3724c74b2e4c30f10a633443e600ae7b
Alexandr <Zerling14@gmail.com>
2017-02-25 13:15:34 +0700

    Replace variable name time to depDay

--- main.c
+++ main.c
@@ -1,31 +1,31 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int CheckInput(int time, int sum)
+int CheckInput(int depDay, int sum)
 {
-	if ((time >= 0) && (time <= 365) && (sum >= 10000))
+	if ((depDay >= 0) && (depDay <= 365) && (sum >= 10000))
 		return 1;
 	return 0;
 }
 
-int CalcSumDeposit(int time, int sum)
+int CalcSumDeposit(int depDay, int sum)
 {
 	float bonusPercent = 0;
 	if (sum >= 100000)
 		bonusPercent = 0.01;
-	if (time <= 30)
+	if (depDay <= 30)
 	{
 		sum *= 0.9;
 	}
-	else if (time <= 120)
+	else if (depDay <= 120)
 	{
 		sum *= 1.0 + 0.02 + bonusPercent * 1;
 	}
-	else if (time <= 240)
+	else if (depDay <= 240)
 	{
 		sum *= 1.0 + 0.6 + bonusPercent * 2;
 	}
-	else if (time <= 365)
+	else if (depDay <= 365)
 	{
 		sum *= 1.0 + 0.12 + bonusPercent * 3;
 	}
@@ -34,19 +34,19 @@ int CalcSumDeposit(int time, int sum)
 
 int main()
 {
-	int time, sum;
+	int depDay, sum;
 	printf("Введите срок вклада: ");
-	scanf("%d", &time);
+	scanf("%d", &depDay);
 	printf("Введите сумму вклада: ");
 	scanf("%d", &sum);
 
-	if (CheckInput(time, sum) == 0)
+	if (CheckInput(depDay, sum) == 0)
 	{
 		printf("Некоректный ввод\n");
 		return 1;
 	}
 
-	printf("%d", CalcSumDeposit(time, sum));
+	printf("%d", CalcSumDeposit(depDay, sum));
 
 	return 0;
 }
