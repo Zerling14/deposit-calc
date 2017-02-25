5ce1268ac843cd9617b5d7e6a696158b40c290a7
Alexandr <Zerling14@gmail.com>
2017-02-25 12:53:29 +0700

    Add func CalcSumDeposit

--- main.c
+++ main.c
@@ -8,6 +8,30 @@ int CheckInput(int time, int sum)
 	return 0;
 }
 
+int CalcSumDeposit(int time, int sum)
+{
+	float bonusPercent = 0;
+	if (sum >= 100000)
+		bonusPercent = 0.01;:
+	if (time <= 30)
+	{
+		sum *= 0.9;
+	}
+	else if (time <= 120)
+	{
+		sum *= 1.0 + 0.02 + bonusPercent * 1;
+	}
+	else if (time <= 240)
+	{
+		sum *= 1.0 + 0.6 + bonusPercent * 2;
+	}
+	else if (time <= 365)
+	{
+		sum *= 1.0 + 0.12 + bonusPercent * 3;
+	}
+	return sum;
+}
+
 int main()
 {
 	int time, sum, percent;
