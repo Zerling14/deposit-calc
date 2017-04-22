0663501834febc55c1db2914248ec5e4b44efa56
Zerling14 <Zerling14@gmail.com>
2017-04-22 11:13:07 +0700

    Add test for CalcSumDeposit

--- test/deposit_test.c
+++ test/deposit_test.c
@@ -1,12 +1,164 @@
 #include "ctest.h"
 #include "deposit.h"
 
-CTEST(arithmetic_suite, simle_sum)
+CTEST(CalcSumDeposit, calc_sum_0_less_100k)
 {
-	const int day = 20;
-	const int dep = 30000;
+	const int day = 0;
+	const int dep = 99999;
 	const int result = CalcSumDeposit(day, dep);
 	
-	const int expect = 27000;
-	ASSERT_DBL_NEAR(expect, result);
+	const int expect = 89999;
+	ASSERT_EQUAL(expect, result);
 }
+
+CTEST(CalcSumDeposit, calc_sum_0_more_100k)
+{
+	const int day = 0;
+	const int dep = 100000;
+	const int result = CalcSumDeposit(day, dep);
+	
+	const int expect = 90000;
+	ASSERT_EQUAL(expect, result);
+}
+
+CTEST(CalcSumDeposit, calc_sum_30_less_100k)
+{
+	const int day = 30;
+	const int dep = 99999;
+	const int result = CalcSumDeposit(day, dep);
+	
+	const int expect = 89999;
+	ASSERT_EQUAL(expect, result);
+}
+
+CTEST(CalcSumDeposit, calc_sum_30_more_100k)
+{
+	const int day = 30;
+	const int dep = 100000;
+	const int result = CalcSumDeposit(day, dep);
+	
+	const int expect = 90000;
+	ASSERT_EQUAL(expect, result);
+}
+
+CTEST(CalcSumDeposit, calc_sum_31_less_100k)
+{
+	const int day = 31;
+	const int dep = 99999;
+	const int result = CalcSumDeposit(day, dep);
+	
+	const int expect = 101998;
+	ASSERT_EQUAL(expect, result);
+}
+
+CTEST(CalcSumDeposit, calc_sum_31_more_100k)
+{
+	const int day = 31;
+	const int dep = 100000;
+	const int result = CalcSumDeposit(day, dep);
+	
+	const int expect = 103000;
+	ASSERT_EQUAL(expect, result);
+}
+
+CTEST(CalcSumDeposit, calc_sum_120_less_100k)
+{
+	const int day = 120;
+	const int dep = 99999;
+	const int result = CalcSumDeposit(day, dep);
+	
+	const int expect = 101998;
+	ASSERT_EQUAL(expect, result);
+}
+
+CTEST(CalcSumDeposit, calc_sum_120_more_100k)
+{
+	const int day = 120;
+	const int dep = 100000;
+	const int result = CalcSumDeposit(day, dep);
+	
+	const int expect = 103000;
+	ASSERT_EQUAL(expect, result);
+}
+
+CTEST(CalcSumDeposit, calc_sum_121_less_100k)
+{
+	const int day = 121;
+	const int dep = 99999;
+	const int result = CalcSumDeposit(day, dep);
+	
+	const int expect = 105998;
+	ASSERT_EQUAL(expect, result);
+}
+
+CTEST(CalcSumDeposit, calc_sum_121_more_100k)
+{
+	const int day = 121;
+	const int dep = 100000;
+	const int result = CalcSumDeposit(day, dep);
+	
+	const int expect = 108000;
+	ASSERT_EQUAL(expect, result);
+}
+
+CTEST(CalcSumDeposit, calc_sum_240_less_100k)
+{
+	const int day = 240;
+	const int dep = 99999;
+	const int result = CalcSumDeposit(day, dep);
+	
+	const int expect = 105998;
+	ASSERT_EQUAL(expect, result);
+}
+
+CTEST(CalcSumDeposit, calc_sum_240_more_100k)
+{
+	const int day = 240;
+	const int dep = 100000;
+	const int result = CalcSumDeposit(day, dep);
+	
+	const int expect = 108000;
+	ASSERT_EQUAL(expect, result);
+}
+
+CTEST(CalcSumDeposit, calc_sum_241_less_100k)
+{
+	const int day = 241;
+	const int dep = 99999;
+	const int result = CalcSumDeposit(day, dep);
+	
+	const int expect = 111998;
+	ASSERT_EQUAL(expect, result);
+}
+
+CTEST(CalcSumDeposit, calc_sum_241_more_100k)
+{
+	const int day = 241;
+	const int dep = 100000;
+	const int result = CalcSumDeposit(day, dep);
+	
+	const int expect = 115000;
+	ASSERT_EQUAL(expect, result);
+}
+
+CTEST(CalcSumDeposit, calc_sum_365_less_100k)
+{
+	const int day = 365;
+	const int dep = 99999;
+	const int result = CalcSumDeposit(day, dep);
+	
+	const int expect = 111998;
+	ASSERT_EQUAL(expect, result);
+}
+
+CTEST(CalcSumDeposit, calc_sum_365_more_100k)
+{
+	const int day = 365;
+	const int dep = 100000;
+	const int result = CalcSumDeposit(day, dep);
+	
+	const int expect = 115000;
+	ASSERT_EQUAL(expect, result);
+}
+
+
